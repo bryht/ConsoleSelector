c7d2e1bccf6f3408eeaad7da758b78d2a8b686e9
liming <bryht@live.com>
2017-05-08 11:31:15 +0800

    add input string, int , bool

---
+++ ConsoleApp1/ConsoleHelper.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleSelector
+{
+     class ConsoleHelper
+    {
+
+        public static string InputString(string title) {
+            Console.WriteLine(title);
+            var result = Console.ReadLine();
+            return result;
+        }
+
+        public static int InputInt(string title) {
+            var result = 0;
+            var readStr = string.Empty;
+            var flag = true;
+            while (flag)
+            {
+                Console.WriteLine(title);
+                readStr = Console.ReadLine();
+                if (Int32.TryParse(readStr, out result))
+                {
+                    flag = false;
+                }
+            }
+
+            return result;
+        }
+
+        public static bool InputBool(string title) {
+            Console.WriteLine(title);
+            var result = Console.ReadKey();
+            switch (result.Key)
+            {
+                case ConsoleKey.Y:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public static int SingleOptions(string title, string[] options)
+        {
+            Console.WriteLine(title + ":");
+            for (int i = 0; i < options.Length; i++)
+            {
+                Console.WriteLine("[ ]" + (i + 1) + ". " + options[i]);
+            }
+
+            var topNum = Console.CursorTop - options.Length;
+            var bottomNum = Console.CursorTop;
+            var chooseIndex = 0;
+            Console.CursorTop = topNum;
+            Console.Write("[*");
+            Console.CursorLeft = 0;
+            bool flag = true;
+            while (flag)
+            {
+                var key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (Console.CursorTop > topNum)
+                        {
+                            Console.Write("[ ");
+                            Console.CursorLeft = 0;
+                            Console.CursorTop--;
+                            chooseIndex--;
+                            Console.Write("[*");
+                            Console.CursorLeft = 0;
+                        }
+
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (Console.CursorTop < bottomNum - 1)
+                        {
+                            Console.Write("[ ");
+                            Console.CursorLeft = 0;
+                            Console.CursorTop++;
+                            chooseIndex++;
+                            Console.Write("[*");
+                            Console.CursorLeft = 0;
+                        }
+                        break;
+
+                    case ConsoleKey.Enter:
+                        flag = false;
+                        Console.CursorTop = bottomNum;
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return chooseIndex;
+
+        }
+
+        public static List<int> MultipleOptions(string title, string[] options)
+        {
+
+            Console.WriteLine(title + ":");
+            for (int i = 0; i < options.Length; i++)
+            {
+                Console.WriteLine("[ ]" + (i + 1) + ". " + options[i]);
+            }
+
+            var topNum = Console.CursorTop - options.Length;
+            var bottomNum = Console.CursorTop;
+            var currentIndex = 0;
+            var chooseIndex = new List<int>();
+            Console.CursorTop = topNum;
+
+            bool flag = true;
+            while (flag)
+            {
+                var key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (Console.CursorTop > topNum)
+                        {
+                            Console.CursorTop--;
+                            currentIndex--;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (Console.CursorTop < bottomNum - 1)
+                        {
+                            Console.CursorTop++;
+                            currentIndex++;
+                        }
+                        break;
+
+                    case ConsoleKey.Spacebar:
+
+                        if (chooseIndex.Contains(currentIndex))
+                        {
+                            chooseIndex.Remove(currentIndex);
+                            Console.Write("[ ");
+                            Console.CursorLeft = 0;
+                        }
+                        else
+                        {
+                            chooseIndex.Add(currentIndex);
+                            Console.Write("[*");
+                            Console.CursorLeft = 0;
+                        }
+
+                        break;
+                    case ConsoleKey.Enter:
+                        flag = false;
+                        Console.CursorTop = bottomNum;
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return chooseIndex;
+
+        }
+
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
 namespace ConsoleSelector
 {
 
@@ -16,17 +17,22 @@ namespace ConsoleSelector
             Console.WriteLine();
 
             string[] options = { "aaaa", "bbbb", "ccccc", "ddddd", "eeeee" };
-            var chooseIndex = SingleOptions("Please choose the right one", options);
-            Console.WriteLine("You choose:" + options[chooseIndex]);
+            var chooseIndex = ConsoleHelper.SingleOptions("Please choose the right one", options);
+            Console.WriteLine("Your choose is:" + options[chooseIndex]);
 
             Console.WriteLine();
 
             string[] options1 = { "111", "222", "333", "444", "555" };
-            var chooseItems = MultipleOptions("Please choose the right one with space", options1);
+            var chooseItems = ConsoleHelper.MultipleOptions("Please choose the right one with space", options1);
             foreach (var item in chooseItems)
             {
-                Console.WriteLine("You choose:" + options1[item]);
+                Console.WriteLine("Your choose are:" + options1[item]);
             }
+
+            Console.WriteLine();
+
+            var intInput = ConsoleHelper.InputInt("Please input a int num:");
+            Console.WriteLine("Your input num is:"+intInput);
 
 
             Console.WriteLine();
@@ -36,129 +42,5 @@ namespace ConsoleSelector
 
         }
 
-        static int SingleOptions(string title, string[] options)
-        {
-
-            Console.WriteLine(title + ":");
-            for (int i = 0; i < options.Length; i++)
-            {
-                Console.WriteLine("[ ]" + (i + 1) + ". " + options[i]);
-            }
-
-            var topNum = Console.CursorTop - options.Length;
-            var bottomNum = Console.CursorTop;
-            var chooseIndex = 0;
-            Console.CursorTop = topNum;
-            Console.Write("[*");
-            Console.CursorLeft = 0;
-            bool flag = true;
-            while (flag)
-            {
-                var key = Console.ReadKey(true);
-                switch (key.Key)
-                {
-                    case ConsoleKey.UpArrow:
-                        if (Console.CursorTop > topNum)
-                        {
-                            Console.Write("[ ");
-                            Console.CursorLeft = 0;
-                            Console.CursorTop--;
-                            chooseIndex--;
-                            Console.Write("[*");
-                            Console.CursorLeft = 0;
-                        }
-
-                        break;
-                    case ConsoleKey.DownArrow:
-                        if (Console.CursorTop < bottomNum - 1)
-                        {
-                            Console.Write("[ ");
-                            Console.CursorLeft = 0;
-                            Console.CursorTop++;
-                            chooseIndex++;
-                            Console.Write("[*");
-                            Console.CursorLeft = 0;
-                        }
-                        break;
-
-                    case ConsoleKey.Enter:
-                        flag = false;
-                        Console.CursorTop = bottomNum;
-
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            return chooseIndex;
-
-        }
-
-        static List<int> MultipleOptions(string title, string[] options)
-        {
-
-            Console.WriteLine(title + ":");
-            for (int i = 0; i < options.Length; i++)
-            {
-                Console.WriteLine("[ ]" + (i + 1) + ". " + options[i]);
-            }
-
-            var topNum = Console.CursorTop - options.Length;
-            var bottomNum = Console.CursorTop;
-            var currentIndex = 0;
-            var chooseIndex = new List<int>();
-            Console.CursorTop = topNum;
-
-            bool flag = true;
-            while (flag)
-            {
-                var key = Console.ReadKey(true);
-                switch (key.Key)
-                {
-                    case ConsoleKey.UpArrow:
-                        if (Console.CursorTop > topNum)
-                        {
-                            Console.CursorTop--;
-                            currentIndex--;
-                        }
-                        break;
-                    case ConsoleKey.DownArrow:
-                        if (Console.CursorTop < bottomNum - 1)
-                        {
-                            Console.CursorTop++;
-                            currentIndex++;
-                        }
-                        break;
-
-                    case ConsoleKey.Spacebar:
-
-                        if (chooseIndex.Contains(currentIndex))
-                        {
-                            chooseIndex.Remove(currentIndex);
-                            Console.Write("[ ");
-                            Console.CursorLeft = 0;
-                        }
-                        else
-                        {
-                            chooseIndex.Add(currentIndex);
-                            Console.Write("[*");
-                            Console.CursorLeft = 0;
-                        }
-
-                        break;
-                    case ConsoleKey.Enter:
-                        flag = false;
-                        Console.CursorTop = bottomNum;
-
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            return chooseIndex;
-
-        }
     }
 }
